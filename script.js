620576279c1cb34cb124b8af7f42c81cab8d83c8
Ritesh kasaudhan <RITESHKASAUDHAN975@GMAIL.COM>
2024-08-03 08:48:27 +0530

    ADD
    DOM
    Document Object Model

---
+++ script.js
@@ -0,0 +1,27 @@
+console.log(window);
+console.log("hello");
+window.console.log("hello2");
+//alert("you are student");
+
+console.dir(window.document);
+console.dir(document.body);
+console.log(document.body);
+console.log(document.head);
+console.log(document.body.childNodes[1]);
+document.getElementById("heading");
+console.log(heading);
+
+//******************* 
+/*
+let headings=document.getElementsByClassName("heading");
+console.dir(headings);
+console.log(heacding);*/
+// let parahs=document.getElementsByTagName("p");
+// console.log(parahs);
+// let element=document.querySelector("p");
+// console.dir(element);
+// let allEl=document.querySelectorAll("h1");
+// console.dir(allEl);
+let firstEl=document.querySelector("p");
+console.dir(firstEl);
+// tegName,innertag,innerText,innerrHTML,textContent
